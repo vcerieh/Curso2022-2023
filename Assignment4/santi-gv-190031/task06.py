c8083f76918f84436b553a1f18ac311640399790
santi-gv <93785971+santi-gv@users.noreply.github.com>
2022-09-30 23:51:54 +0200

    Fixing errors

--- Assignment4/santi-gv-190031/task06.py
+++ Assignment4/santi-gv-190031/task06.py
@@ -9,7 +9,7 @@ Original file is located at
 **Task 06: Modifying RDF(s)**
 """
 
-!pip install rdflib 
+#!pip install rdflib 
 github_storage = "https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2021-2022/master/Assignment4/course_materials"
 
 """Leemos el fichero RDF de la forma que lo hemos venido haciendo"""
@@ -74,7 +74,7 @@ for s, p, o in g:
 # TO DO
 johnUri = ns.JohnSmith
 g.add((ns.UPM, RDF.type, ns.University))
-g.add((johnUri, VCARD.work, ns.UPM))
+g.add((johnUri, VCARD.worksAt, ns.UPM))
 # Visualize the results
 for s, p, o in g:
   print(s,p,o)
